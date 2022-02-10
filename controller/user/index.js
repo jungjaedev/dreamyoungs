ff109813d3ee496ca808efb9e04404522edea691
Jaewon <jungjaedev@gmail.com>
2022-02-10 22:54:33 +0900

    Add : updateHasCat router and function created

--- controller/user/index.js
+++ controller/user/index.js
@@ -45,4 +45,31 @@ module.exports = {
       return res.status(400).json({ message: 'sever error' });
     }
   },
+  updateHasCat: async (req, res) => {
+    try {
+      const index = req.params.userIndex;
+      const user = await Users.findOne({
+        raw: true,
+        where: {
+          index,
+        },
+      });
+      if (!user) {
+        res.status(404).json({ message: `user doesn't exist` });
+      }
+      const updatedData = { ...user, hasCat: !user.hasCat };
+      await Users.update(
+        { ...updatedData },
+        {
+          where: {
+            index,
+          },
+        }
+      );
+      return res.json({ message: 'successfully updated' });
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json({ message: 'sever error' });
+    }
+  },
 };
--- router/user.js
+++ router/user.js
@@ -5,5 +5,6 @@ const { userController } = require('../controller');
 router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 router.get('/:userIndex/hasCat', userController.checkHasCat);
+router.post('/:userIndex/hasCat', userController.updateHasCat);
 
 module.exports = router;
